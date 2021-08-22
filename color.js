71d35c6e9ccbe1df841540a90b82b487a8b94ea8
mockchon <79964231+mockchon@users.noreply.github.com>
2021-08-22 14:15:05 +0900

    day night mode change button 추가

---
+++ color.js
@@ -0,0 +1,37 @@
+var Links={
+  setColor:function(color){
+    var alist=document.querySelectorAll('a');
+    var i=0;
+    while(i<alist.length){
+      alist[i].style.color=color; //매개변수에 괄호 안씀
+      i=i+1;
+    }
+  }
+}
+
+var Body={
+  setColor: function(color){
+    document.querySelector('body').style.color = color; //매개변수에 괄호 없음
+  },
+  setBackgroundColor:function(color){
+    document.querySelector('body').style.backgroundColor = color; //매개변수에 괄호 안쓴다
+  }
+}
+
+function nightDayHandler(self){
+  var target =document.querySelector('body');
+  if (self.value === 'night' ) {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+
+  } else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+    }
+  }
