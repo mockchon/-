539b0b03667574cf2113c5ae3872491249b134c5
mockchon <79964231+mockchon@users.noreply.github.com>
2021-08-22 20:52:56 +0900

    jQuery 라이브러리 사용하여 code update

--- color.js
+++ color.js
@@ -1,20 +1,23 @@
 var Links={
   setColor:function(color){
-    var alist=document.querySelectorAll('a');
-    var i=0;
-    while(i<alist.length){
-      alist[i].style.color=color; //매개변수에 괄호 안씀
-      i=i+1;
-    }
+    // var alist=document.querySelectorAll('a');
+    // var i=0;
+    // while(i<alist.length){
+    //   alist[i].style.color=color; //매개변수에 괄호 안씀
+    //   i=i+1;
+    // }
+      $('a').css('color',color);
   }
 }
 
 var Body={
   setColor: function(color){
-    document.querySelector('body').style.color = color; //매개변수에 괄호 없음
+    // document.querySelector('body').style.color = color; //매개변수에 괄호 없음
+    $('body').css('color',color);
   },
   setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor = color; //매개변수에 괄호 안쓴다
+    // document.querySelector('body').style.backgroundColor = color; //매개변수에 괄호 안쓴다
+    $('body').css('backgroundColor',color);
   }
 }
 
